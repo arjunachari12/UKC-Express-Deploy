ed0c8fb74747f69650eb9a61c88902bf6f06bb6e
Phil Americus <36591787+Philamericus@users.noreply.github.com>
2019-09-19 16:02:08 +0300

    Text updates

--- terraform/UKC_variables.tf
+++ terraform/UKC_variables.tf
@@ -1,15 +1,15 @@
-variable "access_key" { default = "!!!!!! replace it with access_key !!!!!!" }
-variable "secret_key" { default = "!!!!!! replace it with secret key !!!!!!" }
+variable "access_key" { default = "<Access Key>" }
+variable "secret_key" { default = "<Secret Key>" }
 variable "resource-group-name" { default = "Terraform-UKC-demo"}
-variable "ep_public_key_path" { default = "/home/!!!!!! replace it with your user !!!!!!/.ssh/!!!!!! replace it with public key !!!!!!" }
-variable "ep_private_key_path" { default = "/home/!!!!!! replace it with your user !!!!!!/.ssh/!!!!!! replace it with public key !!!!!!" }
-variable "partner_public_key_path" { default = "/home/!!!!!! replace it with your user  !!!!!!/.ssh/!!!!!! replace it with public key!!!!!!" }
-variable "partner_private_key_path" { default = "/home/!!!!!! replace it with your user  !!!!!!/.ssh/!!!!!! replace it with public key !!!!!!" }
+variable "ep_public_key_path" { default = "/home/<your user>/.ssh/<public key>" }
+variable "ep_private_key_path" { default = "/home/<your user>/.ssh/<public key>" }
+variable "partner_public_key_path" { default = "/home/<your user>/.ssh/<public key>" }
+variable "partner_private_key_path" { default = "/home/<your user>/.ssh/<public key>" }
 variable "key_name_0" { default = "Terraform-UKC-EP-key" }
 variable "key_name_1" { default = "Terraform-UKC-Partner-AUX-key" }
 variable "password1" { default = "Password1!" }
-variable "aws_region" { default = "sa-east-1" }
-variable "ukc_rpm" { default = "ekm-2.0.1905.36686-RHES.x86_64.rpm" }
+variable "aws_region" { default = "<location of amazon cloud, e.g. sa-east-1>" }
+variable "ukc_rpm" { default = "<Name of UKC package, e.g. ekm-2.0.1905.36686-RHES.x86_64.rpm>" }
 variable "os_user_0" {default = "centos" }
 
 
